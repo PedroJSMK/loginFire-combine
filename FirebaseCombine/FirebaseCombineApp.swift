8d18906366c95cdc77c922d3d7ec0eb9e195b1f3
PJSMK <pedrokanagusto@hotmail.com>
2022-01-21 13:40:41 -0300

    Initial Commit

---
+++ FirebaseCombine/FirebaseCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseCombineApp.swift
+//  FirebaseCombine
+//
+//  Created by Pedro Kanagusto on 21/01/22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
